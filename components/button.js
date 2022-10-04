a553be455c492056ca7c667ed5122a6c091de762
singularity0614 <bingbongrice@gmail.com>
2022-10-04 21:24:17 +1100

    updated stock display

--- components/button.js
+++ components/button.js
@@ -1,8 +1,21 @@
-function StockDisplay(hello) {
+function StockDisplay(props) {
     return (
         <>
-            <div className="flex justify-center items-center w-[80vw] rounded-xl p-1 mx-auto my-4 max-w-sm bg-gradient-to-r from-[#049cb7] to-[#3dd9bd]">
-                <div className="bg-white p-5 rounded-lg w-[80vw]">Y</div>
+            <div className="flex justify-center items-center w-[60vw] rounded-xl p-0.5 mx-auto my-4 bg-gradient-to-r from-[#bdc3c7] to-[#7d868f]">
+                <div className="flex justify-between items-center bg-white px-6 py-5 rounded-[10px] w-[60vw]">
+                    <div>
+                        <div>{props.stockName}</div>
+                        <div className="text-sm text-slate-500">{props.stockCode}</div>
+                    </div>
+                    <div className="flex justify-between items-center w-64 font-medium">
+                        <div className="p-1.5">$94.45</div>
+                        <div className="text-[#137333]">+$1.17</div>
+                        <div className="flex justify-evenly items-center p-1.5 rounded-lg text-[#137333] bg-[#e8f4eb]">
+                            <svg width="16" height="16" viewBox="0 0 24 24" focusable="false" className="fill-[#137333]"><path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"></path></svg>
+                            <div>1.25%</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
--- pages/school/computing/independent/jerry/index.js
+++ pages/school/computing/independent/jerry/index.js
@@ -11,7 +11,7 @@ export default function Home() {
             <main className="font-proxima">
                 <header>
                     <div className="my-6 flex justify-center items-center">
-                        <h1 className="font-extrabold text-7xl text-transparent bg-clip-text bg-gradient-to-r from-[#049cb7] to-[#3dd9bd]">Stock Dashboard</h1>
+                        <h1 className="font-bold text-7xl text-transparent bg-clip-text bg-gradient-to-r from-[#049cb7] to-[#3dd9bd]">Stock Dashboard</h1>
                     </div>
                 </header>
                 <section>
@@ -21,10 +21,8 @@ export default function Home() {
                 </section>
                 <section>
                     <div className="flex justify-center flex-col items-center">
-                        <Button></Button>
-                        <Button></Button>
-                        <Button></Button>
-                        <Button></Button>
+                        <Button stockName="S&P/ASX 200" stockCode="XJO"></Button>
+                        <Button stockName="Rio Tinto" stockCode="RIO"></Button>
                     </div>   
                 </section>
                 <footer>
