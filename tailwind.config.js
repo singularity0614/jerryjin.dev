174371960cd4b49213b1f9a8d7e0e72dbbb2c00d
singularity0614 <bingbongrice@gmail.com>
2022-10-12 23:12:22 +1100

    added keyframe animation to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      transitionProperty: {
+        maxHeight: 'max-height',
+        margin  : 'margin',
+      },
       fontFamily: {
         'proximanova': ['proxima-nova', 'sans-serif'],
         sans: [
@@ -36,20 +40,59 @@ module.exports = {
             transform: 'translate3d(0, 0, 10px) scale(1)',
           }
         },
-        'slide-in-from-top': {
+        'fade-in': {
           '0%': {
-            
+            opacity: '0',
           },
-          '99%': {
-
+          '40%': {
+            opacity: '0',
           },
           '100%': {
-
+            opacity: '1',
           }
+        },
+        'fade-out': {
+          '0%': {
+            opacity: '1',
+          },
+          '40%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '0',
+          }
+        },
+        'slide-in-from-top': {
+          '0%': {
+            margin: '0',
+            maxHeight: '0px',
+            transform: 'translate3d(0, -50px, 0) scale(1)',
+          },
+          '100%': {
+            margin: '0.5rem auto',
+            maxHeight: '200px',
+            transform: 'translate3d(0, 0, 0) scale(1)',
+          },
+        },
+        'slide-out-to-top': {
+          '0%': {
+            margin: '0.5rem auto',
+            maxHeight: '200px',
+            transform: 'translate3d(0, 0px, 0) scale(1)',
+          },
+          '100%': {
+            margin: '0',
+            maxHeight: '0px',
+            transform: 'translate3d(0, -50px, 0) scale(1)',
+          },
         },
       },
       animation: {
         'fade-in-from-bottom': 'fade-in-from-bottom 800ms cubic-bezier(0.4, 0, 0.25, 1)',
+        'fade-in': 'fade-in 800ms cubic-bezier(0.4, 0, 0.25, 1) forwards',
+        'fade-out': 'fade-out 800ms cubic-bezier(0.4, 0, 0.25, 1) forwards',
+        'slide-in-from-top': 'slide-in-from-top 1000ms forwards',
+        'slide-out-to-top': 'slide-out-to-top 1000ms forwards',
       }
     },
   },
