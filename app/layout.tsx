2a572b05afdd2fdba1e6a03dade94468d992100b
singularity0614 <bingbongrice@gmail.com>
2023-01-17 16:55:06 +1100

    small edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,6 @@ const inter = Inter({
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2596207444336931" crossOrigin="anonymous" />
             <body className={`${inter.variable} font-sans bg-[#F8FAFC] text-[#222222] dark:bg-black dark:text-[#F8FAFC] selection:bg-[#222222] selection:text-[#F8FAFC] dark:selection:bg-[#F8FAFC] dark:selection:text-black`}>
                 <ThemeProvider enableSystem={true} attribute="class">
                     {children}
