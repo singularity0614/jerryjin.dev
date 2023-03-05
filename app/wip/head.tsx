9d92eaf682a66d1cfe9b85113c1da765c4545768
singularity0614 <bingbongrice@gmail.com>
2023-03-05 18:06:38 +1100

    deleted wip page

--- app/wip/head.tsx
+++
@@ -1,8 +0,0 @@
-export default function Head() {
-    return (
-        <>         
-            <title>Working In Progress</title>
-            <meta name="description" content="Come back later for more!" />
-        </>
-    )
-}
