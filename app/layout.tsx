7f178e1e2f05d6d479598c8cf834a2cbcf1ccacf
singularity0614 <bingbongrice@gmail.com>
2023-03-26 12:35:34 +1100

    brought back next/font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,19 @@
 
 import "./globals.css";
 import Script from "next/script";
+import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+
+const inter = Inter({
+    subsets: ['latin'],
+    variable: '--font-inter',
+})
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2596207444336931" crossOrigin="anonymous" />
-            <body className={`font-sans bg-[#F8FAFC] text-[#222222] dark:bg-black dark:text-[#F8FAFC] selection:bg-[#222222] selection:text-[#F8FAFC] dark:selection:bg-[#F8FAFC] dark:selection:text-black`}>
+            <body className={`${inter.variable} font-sans bg-[#F8FAFC] text-[#222222] dark:bg-black dark:text-[#F8FAFC] selection:bg-[#222222] selection:text-[#F8FAFC] dark:selection:bg-[#F8FAFC] dark:selection:text-black`}>
                 <ThemeProvider enableSystem={true} attribute="class">
                     {children}
                 </ThemeProvider>
