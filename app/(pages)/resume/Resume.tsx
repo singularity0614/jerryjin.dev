1ff0e55e87082c351b65930dc0a7ff6c725c2747
singularity0614 <bingbongrice@gmail.com>
2023-05-22 00:00:01 +1000

    new blog + reverted resume

--- app/(pages)/resume/Resume.tsx
+++ app/(pages)/resume/Resume.tsx
@@ -6,33 +6,20 @@ export default function ResumeContents() {
     const [slideNum, setSlideNum] = useState(0);
     const totalSlides = 2;
 
-    const refs = Array(totalSlides+1).fill(useRef());
-
-    const scrollToSlide = (i: number) => {
-        refs[i].current.scrollIntoView({
-            behavior: 'smooth',
-            block: 'nearest',
-            inline: 'start',
-        });
-    }
-
     const prevSlide = () => {
         setSlideNum(Math.max(0, slideNum-1));
-        scrollToSlide(slideNum);
     }
     
     const nextSlide = () => {
         setSlideNum(Math.min(totalSlides, slideNum+1))
-        scrollToSlide(slideNum);
     }
 
     const Slide = ({number, children}) => {
+
         return (
             <>
-                <div className="relative w-full" ref={refs[number]}>
-                    <div className="w-[576px] md:w-[75vw]">
-                        {children}
-                    </div>
+                <div className={`${slideNum === number ? "block" : "hidden"}`}>
+                    {children}
                 </div>
             </>
         )
@@ -59,35 +46,33 @@ export default function ResumeContents() {
                     </button>
                 </div>
             </div>
-            <div className="overflow-hidden flex relative">
-                <Slide number={0}>
-                    <h3 className="font-medium text-5xl mt-24 mb-16">Education</h3>
-                    <table className="table-fixed w-full mb-16">
-                        <tbody>
-                            <tr className="border-b-2 border-b-slate-200 dark:border-b-[#222222]">
-                                <td className="font-light text-lg pt-6 pb-12 w-56">2021-present</td>
-                                <td className="text-2xl pt-6 pb-12">Camberwell Grammar School</td>
-                            </tr>
-                            <tr className="border-b-2 border-b-slate-200 dark:border-b-[#222222]">
-                                <td className="font-light text-lg pt-6 pb-12">2020</td>
-                                <td className="text-2xl pt-6 pb-12">Caulfield Grammar School</td>
-                            </tr>
-                            <tr className="border-b-2 border-b-slate-200 dark:border-b-[#222222]">
-                                <td className="font-light text-lg pt-6 pb-12">2010-2019</td>
-                                <td className="text-2xl pt-6 pb-12">Wesley College</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </Slide>
-                <Slide number={1}>
-                    <h3 className="font-medium text-5xl mt-24 mb-12">Experience</h3>
-                    N/A looks pretty bad haha
-                </Slide>
-                <Slide number={2}>
-                    <h3 className="font-medium text-5xl mt-24 mb-12">Achievements</h3>
-                    N/A for now as well
-                </Slide>
-            </div>
+            <Slide number={0}>
+                <h3 className="font-medium text-5xl mt-24 mb-16">Education</h3>
+                <table className="table-fixed w-full mb-16">
+                    <tbody>
+                        <tr className="border-b-2 border-b-slate-200 dark:border-b-[#222222]">
+                            <td className="font-light text-lg pt-6 pb-12 w-56">2021-present</td>
+                            <td className="text-2xl pt-6 pb-12">Camberwell Grammar School</td>
+                        </tr>
+                        <tr className="border-b-2 border-b-slate-200 dark:border-b-[#222222]">
+                            <td className="font-light text-lg pt-6 pb-12">2020</td>
+                            <td className="text-2xl pt-6 pb-12">Caulfield Grammar School</td>
+                        </tr>
+                        <tr className="border-b-2 border-b-slate-200 dark:border-b-[#222222]">
+                            <td className="font-light text-lg pt-6 pb-12">2010-2019</td>
+                            <td className="text-2xl pt-6 pb-12">Wesley College</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </Slide>
+            <Slide number={1}>
+                <h3 className="font-medium text-5xl mt-24 mb-12">Experience</h3>
+                Stuff coming soon...
+            </Slide>
+            <Slide number={2}>
+                <h3 className="font-medium text-5xl mt-24 mb-12">Achievements</h3>
+                (Very limited) stuff coming soon...
+            </Slide>
         </>
     )
 }
--- app/(pages)/resume/page.tsx
+++ app/(pages)/resume/page.tsx
@@ -11,7 +11,7 @@ export default function Resume() {
     return (
         <>
             <div className="flex justify-center mt-[10vw] xl:mt-32 mb-16">
-                <div className="">
+                <div className="w-[576px] md:w-[75vw]">
                     <ResumeContents></ResumeContents>
                 </div>
             </div>
